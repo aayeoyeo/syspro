53237cf16603cee83008d343c15d46bef2be7f99
taeinYeo <cmsk120@gmail.com>
2025-09-23 14:10:06 +0000

    my first code

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -2,32 +2,30 @@
 #include "copy.h"
 #include <string.h>
 
-#define MAX_LINES 5
-#define MAXLINE 100
+char line[MAXLINE];
+char longest[5][MAXLINE];
 
 int main() {
-    char lines[MAX_LINES][MAXLINE];
-    int lengths[MAX_LINES];
-    int i = 0;
-    while (i < MAX_LINES) {
-        if (fgets(lines[i], MAXLINE, stdin) == NULL) break;
-        lengths[i] = strlen(lines[i]);
-        i++;
-    }
-
-    for (i = 0; i < MAX_LINES - 1; i++) {
-        for (int j = 0; j < MAX_LINES - 1 - i; j++) {
-            if (lengths[j] < lengths[j + 1]) {
-                int temp_len = lengths[j];
-                lengths[j] = lengths[j + 1];
-                lengths[j + 1] = temp_len;
-                char temp_str[MAXLINE];
-                copy(lines[j], temp_str);
-                copy(lines[j + 1], lines[j]);
-                copy(temp_str, lines[j + 1]);
-            }
-        }
-    }
-    for (i = 0; i < MAX_LINES; i++) printf("%s", lines[i]);
-    return 0;
+	int len;
+	int max;
+	max = 0;
+	
+	int i = 0;
+	while(i < 5) {
+		gets(longest[i]);
+		i++;
+	}
+	
+	for (i = 0; i < 4; i++) {
+		for (int j = 0; j < 4 - i; j++) {
+			if (strlen(longest[j]) < strlen(longest[j + 1])) {
+				char change[MAXLINE];
+				copy(longest[j], change);
+				copy(longest[j + 1], longest[j]);
+				copy(change, longest[j + 1]);
+			}
+		}
+	}
+	for (i = 0; i < 5; i++) printf("%s\n", longest[i]);
+	return 0;
 }
