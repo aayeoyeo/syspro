d401cdf5d20caad13be9eb8464f4a83ebc6f42f0
taeinYeo <cmsk120@gmail.com>
2025-12-02 04:53:37 +0000

    chap11

--- chap11/hw1/almhandler.c
+++ chap11/hw1/almhandler.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
+
+struct sigaction newact;
+struct sigaction oldact;
 void alarmHandler();
+
+void mysignal(int signo, void (*handler)(int))
+{
+    newact.sa_handler = handler;
+    sigfillset(&newact.sa_mask);
+    newact.sa_flags = 0;
+    sigaction(signo, &newact, &oldact);
+}
 
 int main()
 {
-	signal(SIGALRM, alarmHandler);
-	alarm(5);
-	short i = 0;
-	while (1) {
-		sleep(1);
-		i++;
-		printf("%d second\n", i);
-	}
-	printf("end\n");
+    mysignal(SIGALRM, alarmHandler);
+    alarm(5);
+    short i = 0;
+    while (1) {
+        sleep(1);
+        i++;
+        printf("%d second\n", i);
+    }
+    printf("end\n");
 }
 
 void alarmHandler(int signo)
 {
-	printf("Wake up\n");
-	exit(0);
+    printf("Wake up\n");
+    exit(0);
 }
